ec451e8644de788f5c1bcbd6c4ad60caa204d568
socia <social@axvleda.de>
2020-10-05 15:16:19 +0200

    Homework Update

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -15,14 +15,15 @@ public class BaseTest {
     public void setUp() {
         System.setProperty("webdriver.gecko.driver", "src\\resources\\drivers\\win64\\geckodriver.exe");
         driver = new FirefoxDriver();
+        driver.manage().deleteAllCookies();
         driver.manage().window().setPosition(new Point(-1685,22));
         driver.manage().window().setSize(new Dimension(850,1015));
-        driver.manage().deleteAllCookies();
 
     }
 
     @AfterClass
     public void afterClass() {
         driver.close();
+        driver.manage().deleteAllCookies();
     }
 }
